26fc5c67f4dbb172fcbafa5ce3d8c2c1fe6fe532
Bahtiyor Esanov <mr.bahtiyoresanov@hotmail.com>
2019-01-21 03:46:04 +0200

    Add project files.

---
+++ VendingMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
